dfd9861e95ed32864cb481fc454f2b8807cb1449
Nicolas <nilonb7@gmail.com>
2023-04-18 19:49:48 -0300

    navbar botones-faltan algunos detalles

--- assets/js/tienda.js
+++ assets/js/tienda.js
@@ -43,7 +43,7 @@ function imprimirCards(productos, elemento) {
                             <p class="texto3-producto m-0">${productosDisponibles}</p>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-around">
+                    <div class="d-flex my-2 justify-content-between">
                         <div class="card-button1 d-flex justify-content-center align-items-center fw-bold" title="Añadir a favoritos">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill corazon-default" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path>
