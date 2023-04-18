5544cb829b18772448715bd5f9a7cc86d7a2d80e
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-04-18 02:25:59 -0300

    dinamismo de botones de producto-carrito de compra

--- assets/js/jugueteria.js
+++ assets/js/jugueteria.js
@@ -3,6 +3,8 @@ fetch("https://mindhub-xj03.onrender.com/api/petshop")
     .then((data) => {
         console.log(data);
         imprimirCards(data);
+        botonDinamico(".card-button1",".corazon-default","corazon-rojo","borde-rojo")
+        botonDinamico(".card-button3",".carrito-default","carrito-verde","borde-verde")
     })
     .catch((error) => {
         console.error(error);
@@ -10,7 +12,7 @@ fetch("https://mindhub-xj03.onrender.com/api/petshop")
 
 function imprimirCards(productos) {
     const contenedor = document.getElementById("contenedor-tarjetas");
-    const cardsHTML = productos.map((producto) => {
+    const producto = productos.map((producto) => {
         let productosDisponibles;
         if (producto.disponibles < 5) {
             productosDisponibles = "Pocas unidades";
@@ -35,14 +37,14 @@ function imprimirCards(productos) {
                     </div>
                     <div class="d-flex justify-content-around">
                         <div class="card-button1 d-flex justify-content-center align-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill corazon-default" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path>
                             </svg>
                         </div>
                         <a href="../pages/detalles.html?id=${producto._id}" class="card-button2 d-flex justify-content-center align-items-center
                         d-flex justify-content-center align-items-center text-decoration-none fw-bold text-black">VER</a>
                         <div class="card-button3 d-flex justify-content-center align-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-fill carrito-default" viewBox="0 0 16 16">
                                 <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"></path>
                             </svg>
                         </div>                            
@@ -51,5 +53,16 @@ function imprimirCards(productos) {
             </div>
         `;
     });
-    contenedor.innerHTML = cardsHTML.join("");
+    contenedor.innerHTML = producto.join("");
+}
+
+function botonDinamico(querySelectorAll1, querySelectorAll2, toggle1, toggle2) {
+    const botonesCarritos = document.querySelectorAll(querySelectorAll1);
+    botonesCarritos.forEach((botonCarrito) => {
+        botonCarrito.addEventListener("click", function () {
+            const corazonIcon = botonCarrito.querySelector(querySelectorAll2);
+            corazonIcon.classList.toggle(toggle1);
+            botonCarrito.classList.toggle(toggle2);
+        });
+    });
 }
